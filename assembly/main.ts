e4f0541c2fd51fa4f8b8f8970af2bd548560108b
Thiago Barcala <thiago.barcala@gmail.com>
2021-09-11 22:33:43 +0200

    Fix contract logic

--- assembly/main.ts
+++ assembly/main.ts
@@ -4,24 +4,24 @@ import { RankingEntry, ranking } from './model';
 const RANKING_SIZE = 10;
 
 export function add_ranking_entry(uuid: u128, score: u32): void {
-    let minIndex = 0;
-    for (let i = 1; i < ranking.length; i++) {
-        if (ranking[i].score >= score) {
-            return;
-        }
-
-        if (ranking[i].score < ranking[minIndex].score) {
-            minIndex = i;
-
-        }
-    }
-
     const newEntry = new RankingEntry(uuid, score);
     if (ranking.length < RANKING_SIZE) {
         ranking.pushBack(newEntry);
-    } else {
-        ranking.replace(minIndex, newEntry);
+        return;
     }
+
+    let minIndex = 0;
+    for (let i = 1; i < ranking.length; i++) {
+        if (ranking[i].score < ranking[minIndex].score) {
+            minIndex = i;
+        }
+    }
+
+    if (ranking[minIndex].score >= score) {
+        return;
+    }
+
+    ranking.replace(minIndex, newEntry);
 }
 
 export function reset_ranking(): void {
